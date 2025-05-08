806cf20a720d86c4730e5f338e471f47941c3b30
srajangupta144 <srajangupta144@gmail.com>
2025-05-08 21:05:32 +0530

    password generator

---
+++ 06-passwordManager/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white shadow-lg">
+        PassWord Generator
+      </h1>
+    </div>
+  )
+}
+
+export default App
